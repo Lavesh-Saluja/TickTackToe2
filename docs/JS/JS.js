954d24be3839bf4a342857a0b3817797cdb0e494
@Lavesh-Saluja <b521032@iiit-nh.ac.in>
2022-07-31 23:31:35 +0530

    worked more on frontEnd

--- docs/JS/JS.js
+++ docs/JS/JS.js
@@ -1,12 +1,12 @@
 
       document.addEventListener('DOMContentLoaded', Call);
-      let x=0;
+      let c=0;
       let flag=0;
         function Call()
         {
           
           document.removeEventListener('DOMContentLoaded', Call);
-        console.log(x);
+    
             let e1=document.getElementById("p1");
             let e2=document.getElementById("p2");
             let e3=document.getElementById("p3");
@@ -16,93 +16,106 @@
             let e7=document.getElementById("p7");
             let e8=document.getElementById("p8");
             let e9=document.getElementById("p9");
-            e1.addEventListener('input',check);
-            e2.addEventListener('input',check);
-            e3.addEventListener('input',check);
-            e4.addEventListener('input',check);
-            e5.addEventListener('input',check);
-            e6.addEventListener('input',check);
-            e7.addEventListener('input',check);
-            e8.addEventListener('input',check);
-            e9.addEventListener('input',check);
+            // e1.addEventListener('input',check);
+            // e2.addEventListener('input',check);
+            // e3.addEventListener('input',check);
+            // e4.addEventListener('input',check);
+            // e5.addEventListener('input',check);
+            // e6.addEventListener('input',check);
+            // e7.addEventListener('input',check);
+            // e8.addEventListener('input',check);
+            // e9.addEventListener('input',check);
+            const mutation=new MutationObserver(check);
+            mutation.observe(e1 ,{subtree:true,childList:true});
+            mutation.observe(e2 ,{subtree:true,childList:true});
+            mutation.observe(e3 ,{subtree:true,childList:true});
+            mutation.observe(e4 ,{subtree:true,childList:true});
+            mutation.observe(e5 ,{subtree:true,childList:true});
+            mutation.observe(e6 ,{subtree:true,childList:true});
+            mutation.observe(e7 ,{subtree:true,childList:true});
+            mutation.observe(e8 ,{subtree:true,childList:true});
+            mutation.observe(e9 ,{subtree:true,childList:true});
+
+            console.log(c+11);
         }
           function check()
           {
-            x++;
-            let x1=document.getElementById("p1").value;
-            let x2=document.getElementById("p2").value;
-            let x3=document.getElementById("p3").value;
-            let x4=document.getElementById("p4").value;
-            let x5=document.getElementById("p5").value;
-            let x6=document.getElementById("p6").value;
-            let x7=document.getElementById("p7").value;
-            let x8=document.getElementById("p8").value;
-            let x9=document.getElementById("p9").value;
-              if(x>=5)
+            console.log("checked");
+            c++;
+            let x1=document.getElementById("p1").innerHTML;
+            let x2=document.getElementById("p2").innerHTML;
+            let x3=document.getElementById("p3").innerHTML;
+            let x4=document.getElementById("p4").innerHTML;
+            let x5=document.getElementById("p5").innerHTML;
+            let x6=document.getElementById("p6").innerHTML;
+            let x7=document.getElementById("p7").innerHTML;
+            let x8=document.getElementById("p8").innerHTML;
+            let x9=document.getElementById("p9").innerHTML;
+              if(c>=5)
               {
 
               if(x1==x2 && x2==x3)
               {
                   flag=1;
-                  if (x1=='x')
+                  if (x1=='X')
               alert("player 1 wins");
-              else if(x1=='o')
+              else if(x1=='O')
              alert("player 2 wins");
               }
               else if(x4==x5 && x5==x6)
               {
                 flag=1;
-                if (x4=='x')
+                if (x4=='X')
               alert("player 1 wins");
-              else if(x4=='o')
+              else if(x4=='O')
              alert("player 2 wins");
               }
               else if(x7==x8 && x8==x9)
               {
                 flag=1;
-                if (x7=='x')
+                if (x7=='X')
               alert("player 1 wins");
-              else if(x7=='o')
+              else if(x7=='O')
              alert("player 2 wins");
               }
               else if(x1==x4 && x4==x7)
               {
                 flag=1;
-                if (x1=='x')
+                if (x1=='X')
               alert("player 1 wins");
-              else if(x1=='o')
+              else if(x1=='O')
              alert("player 2 wins");
               }
               else if(x2==x5 && x5==x8)
               {
                 flag=1;
-                if (x2=='x')
+                if (x2=='X')
               alert("player 1 wins");
-              else if(x2=='o')
+              else if(x2=='O')
              alert("player 2 wins");
               }
               else if(x3==x6 && x6==x9)
               {
                 flag=1;
-                if (x3=='x')
+                if (x3=='X')
               alert("player 1 wins");
-              else if(x3=='o')
+              else if(x3=='O')
              alert("player 2 wins");
               }
               else if(x1==x5 && x5==x9)
               {
                 flag=1;
-                if (x1=='x')
+                if (x1=='X')
               alert("player 1 wins");
-              else if(x1=='o')
+              else if(x1=='O')
              alert("player 2 wins");
               }
              else if(x7==x5 && x5==x3)
               {
                 flag=1;
-                if (x7=='x')
+                if (x7=='X')
               alert("player 1 wins");
-              else if(x7=='o')
+              else if(x7=='O')
              alert("player 2 wins");
               }
             }
@@ -111,7 +124,7 @@
 
           }
 
-          if(x==9 && flag==0)
+          if(c==9 && flag==0)
         alert("TIE");
             
         
--- docs/JS/JS2.js
+++ docs/JS/JS2.js
@@ -2,85 +2,138 @@
     const obj2=document.querySelector(".container2");
     const obj3=document.querySelector(".container3");
     const obj4=document.querySelector(".container4");
-    const obj5=document.querySelector(".container1");
-    const obj6=document.querySelector(".container2");
-    const obj7=document.querySelector(".container3");
-    const obj8=document.querySelector(".container4");
-    const obj9=document.querySelector(".container4");
-    const obj11=document.querySelector(".content1");
-    const obj22=document.querySelector(".content2");
-    const obj33=document.querySelector(".content3");
-    const obj44=document.querySelector(".content4");
-    const obj55=document.querySelector(".content1");
-    const obj66=document.querySelector(".content2");
-    const obj77=document.querySelector(".content3");
-    const obj88=document.querySelector(".content4");
-    const obj99=document.querySelector(".content4");
+    const obj5=document.querySelector(".container5");
+    const obj6=document.querySelector(".container6");
+    const obj7=document.querySelector(".container7");
+    const obj8=document.querySelector(".container8");
+    const obj9=document.querySelector(".container9");
+    const obj11=document.querySelector("#p1");
+    const obj22=document.querySelector("#p2");
+    const obj33=document.querySelector("#p3");
+    const obj44=document.querySelector("#p4");
+    const obj55=document.querySelector("#p5");
+    const obj66=document.querySelector("#p6");
+    const obj77=document.querySelector("#p7");
+    const obj88=document.querySelector("#p8");
+    const obj99=document.querySelector("#p9");
    
     // console.log(obj.innerHTML);
     let x=0;
     
         obj1.addEventListener("click",function(){
             if(x==0)
-        obj11.innerHTML="X";
+            {
+                obj11.innerHTML="X";
+              
+            }
         else 
-        obj11.innerHTML="O";
+        {
+            obj11.innerHTML="O";
+          
+        }
         x=1-x;  
     });
 
     obj2.addEventListener("click",function(){
             if(x==0)
-        obj22.innerHTML="X";
+            {
+                obj22.innerHTML="X";
+                
+            }
         else 
-        obj22.innerHTML="O";
+        {
+            obj22.innerHTML="O";
+          
+        }
         x=1-x;  
     });
 
     obj3.addEventListener("click",function(){
             if(x==0)
-        obj33.innerHTML="X";
+            {
+                obj33.innerHTML="X";
+               
+            }
         else 
-        obj33.innerHTML="O";
+        {
+            obj33.innerHTML="O";
+           
+        }
         x=1-x;  
     });
 
     obj4.addEventListener("click",function(){
             if(x==0)
-        obj44.innerHTML="X";
+            {
+                obj44.innerHTML="X";
+            }
         else 
-        obj44.innerHTML="O";
+        {
+            obj44.innerHTML="O";
+          
+        }
         x=1-x;  
     });
     obj5.addEventListener("click",function(){
             if(x==0)
-        obj55.innerHTML="X";
+            {
+                obj55.innerHTML="X";
+                
+            }
         else 
-        obj55.innerHTML="O";
+        {
+            obj55.innerHTML="O";
+           
+        }
         x=1-x;  
     });obj6.addEventListener("click",function(){
             if(x==0)
-        obj66.innerHTML="X";
+            {
+                obj66.innerHTML="X";
+                
+            }
         else 
-        obj66.innerHTML="O";
+        {
+            obj66.innerHTML="O";
+         
+        }
         x=1-x;  
     });obj7.addEventListener("click",function(){
             if(x==0)
-        obj77.innerHTML="X";
+            {
+                obj77.innerHTML="X";
+            
+            }
         else 
-        obj77.innerHTML="O";
+        {
+            obj77.innerHTML="O";
+          
+        }
         x=1-x;  
     });
     obj8.addEventListener("click",function(){
             if(x==0)
-        obj88.innerHTML="X";
+            {
+                obj88.innerHTML="X";
+               
+            }
         else 
-        obj88.innerHTML="O";
+        {
+            obj88.innerHTML="O";
+          
+        }
         x=1-x;  
     });
     obj9.addEventListener("click",function(){
         if(x==0)
-    obj99.innerHTML="X";
+        {
+            obj99.innerHTML="X";
+          
+        }
     else 
-    obj99.innerHTML="O";
+    {
+        obj99.innerHTML="O";
+       
+    }
     x=1-x;  
 });
